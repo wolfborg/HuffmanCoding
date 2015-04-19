1e250dce6aec4c92af351d07bc9534aa6bbe4dd5
wolfborg <kingderekx@yahoo.com>
2015-04-18 22:14:15 -0400

    Fixed file reading and node creation

--- src/HuffmanCoding.java
+++ src/HuffmanCoding.java
@@ -7,11 +7,62 @@ public class HuffmanCoding
 {
 	public static void main(String[] args)
 	{
+		new HuffmanCoding("test1.txt");
+	}
+	
+	private class Node
+	{
+		char letter;
+		int count;
 		
+		private Node()
+		{
+			new Node('\u0000',0);
+		}
+		
+		private Node(char nodeLetter, int letterCount)
+		{
+			letter = nodeLetter;
+			count = letterCount;
+		}
+		
+		private char getLetter()
+		{
+			return letter;
+		}
+		
+		private int getCount()
+		{
+			return count;
+		}
+		
+		private void setLetter(char newLetter)
+		{
+			letter = newLetter;
+		}
+		
+		private void setCount(int newCount)
+		{
+			count = newCount;
+		}
+		
+		private boolean equals(Node node)
+		{
+			if(letter==node.letter){
+				return true;
+			}
+			
+			return false;
+		}
 	}
 	
 	public HuffmanCoding(String fileName)
 	{
+		BinaryTree<Node> huffman = new BinaryTree<Node>();
+		
+		int[] characters = new int[26];
+		
+		Node[] nodes = new Node[26];
 		Scanner inputStream = null;
 		
 		try{
@@ -24,15 +75,36 @@ public class HuffmanCoding
 		System.out.println("Reading '"+fileName+"' ... ");
 		while(inputStream.hasNextLine()){
 			String line = inputStream.nextLine();
+			line = line.toUpperCase();
 			char[] letters = line.toCharArray();
 			
 			for(int i=0;i<letters.length;i++){
-				if(Character.isAlphabetic(letters[i])){
-					
+				int index = letters[i] - 'A';
+				
+				characters[index]++;
+				
+				for(int j=0;j<nodes.length;j++){
+					if(nodes[j]!=null){
+						if(nodes[j].getLetter()==letters[i]){
+							nodes[j].setCount(characters[index]);
+							break;
+						}
+					}else{
+						nodes[j] = new Node(letters[i],characters[index]);
+						break;
+					}
 				}
 			}
 		}
 		
 		inputStream.close();
+		
+		for(int i=0;i<nodes.length;i++){
+			if(nodes[i]!=null){
+				System.out.println(nodes[i].getLetter()+", "+nodes[i].getCount());
+			}
+		}
+		
+		System.out.println();
 	}
 }
